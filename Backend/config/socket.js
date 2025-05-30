d20cb74ac2e9ca697d5e44b4cf71175d7ce6444b
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2025-05-30 02:22:11 -0700

    Update socket.js

--- Backend/config/socket.js
+++ Backend/config/socket.js
@@ -7,7 +7,7 @@ const userSocketMap = {};
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173'],
+      origin: ['https://chat-web-app-6qam.onrender.com'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization', 'token']
